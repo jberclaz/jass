294018078a3328dcb173956d028274202950c214
Jerome Berclaz <jerome@percipient.ai>
2020-04-06 16:43:14 -0700

    added text antialiasing (#4)

--- src/main/java/com/leflat/jass/client/CanvasBorder.java
+++ src/main/java/com/leflat/jass/client/CanvasBorder.java
@@ -21,17 +21,19 @@ public class CanvasBorder extends JassCanvas {
     }
 
     public void paint(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         Dimension d = getSize();
 
         int w = (d.width - CardImages.IMG_WIDTH) / 2;
         int h = (d.height - hand.size() * Y_STEP - 66) / 2 + 20;
         for (int i = 0; i < hand.size(); i++) {
-            g.drawImage(CardImages.getInstance().getImage(hand.get(i)), w, h + i * 30, this);
+            g2.drawImage(CardImages.getInstance().getImage(hand.get(i)), w, h + i * 30, this);
         }
 
-        g.drawString(name, 20, 30);
+        g2.drawString(name, 20, 30);
         if (atout) {
-            g.drawString("atout", 20, 60);
+            g2.drawString("atout", 20, 60);
         }
     }
 }
--- src/main/java/com/leflat/jass/client/CanvasTop.java
+++ src/main/java/com/leflat/jass/client/CanvasTop.java
@@ -22,16 +22,19 @@ public class CanvasTop extends JassCanvas {
     }
 
     public void paint(Graphics g) {
+        Graphics2D g2 = (Graphics2D) g.create();
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+
         Dimension d = getSize();
 
         int w = (d.width - 36 - hand.size() * X_STEP) / 2;
         for (int i = 0; i < hand.size(); i++) {
-            g.drawImage(CardImages.getInstance().getImage(hand.get(i)), w + X_STEP * i, 20, this);
+            g2.drawImage(CardImages.getInstance().getImage(hand.get(i)), w + X_STEP * i, 20, this);
         }
 
-        g.drawString(name, 30, 15);
+        g2.drawString(name, 30, 15);
         if (atout) {
-            g.drawString("atout", name.length() * 7 + 60, 15);
+            g2.drawString("atout", name.length() * 7 + 60, 15);
         }
     }
 }
