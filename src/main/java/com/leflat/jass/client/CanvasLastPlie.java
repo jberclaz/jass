e0a342ea01ea64058c264094caa37710176e83ad
Jerome Berclaz <jerome.berclaz@a3.epfl.ch>
2020-04-11 08:45:49 -0700

    nit

--- src/main/java/com/leflat/jass/client/CanvasLastPlie.java
+++ src/main/java/com/leflat/jass/client/CanvasLastPlie.java
@@ -29,6 +29,7 @@ public class CanvasLastPlie extends JPanel {
         ourScore = 0;
         theirScore = 0;
         atout = 4;      // ne rien afficher
+        setDoubleBuffered(true);
     }
 
     public void setLastPlie(Collection<Card> plie) {
@@ -53,6 +54,7 @@ public class CanvasLastPlie extends JPanel {
         repaint();
     }
 
+    @Override
     public void paintComponent(Graphics g) {
         Graphics2D g2 = (Graphics2D) g.create();
         g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
